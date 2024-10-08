9dedeaf3cb13e6d32068aa572f644a19738d813b
Tommy Li <dev@stdx.space>
2024-10-08 13:10:55 +0800

    fix: fix prometheus handler not conform to handler route

--- metricsfx/prometheus.go
+++ metricsfx/prometheus.go
@@ -1,8 +1,8 @@
 package metricsfx
 
 import (
-	"net/http"
-
+	"github.com/gin-gonic/gin"
+	"github.com/prismedic/scalpel/routerfx"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -12,10 +12,12 @@ func NewPrometheusHandler() *PrometheusHandler {
 	return &PrometheusHandler{}
 }
 
-func (ph *PrometheusHandler) HttpHandler() http.Handler {
-	return promhttp.Handler()
+func (ph *PrometheusHandler) Handler() gin.HandlerFunc {
+	return gin.WrapH(promhttp.Handler())
 }
 
 func (ph *PrometheusHandler) RoutePattern() string {
 	return "/metrics"
 }
+
+var _ routerfx.HandlerRoute = (*PrometheusHandler)(nil)
